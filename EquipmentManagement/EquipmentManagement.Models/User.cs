343b9ebdb0705192df4cdc2a36279e521fdd03ba
Adriangrz <adriang1412g@gmail.com>
2022-03-17 19:32:01 +0100

    Add user model

---
+++ EquipmentManagement/EquipmentManagement.Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace EquipmentManagement.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+        public string UserType { get; set; }
+    }
+}
