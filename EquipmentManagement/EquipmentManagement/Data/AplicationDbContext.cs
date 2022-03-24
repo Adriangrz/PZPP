5b925550ff136940352c1dc9535159883fa9db58
Adriangrz <adriang1412g@gmail.com>
2022-03-24 18:05:21 +0100

    Add products DbSet

--- EquipmentManagement/EquipmentManagement/Data/AplicationDbContext.cs
+++ EquipmentManagement/EquipmentManagement/Data/AplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace EquipmentManagement.Data
         }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
