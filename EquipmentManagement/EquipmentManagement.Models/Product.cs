0f343b67fd0fb8a186869bc75f414706d44d39a4
Karol6n <karol6n@gmail.com>
2022-03-24 17:55:43 +0100

    Create Product.cs
    Add product model

---
+++ EquipmentManagement/EquipmentManagement.Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace EquipmentManagement.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Desc{ get; set; }
+        public string Cost { get; set; }
+        public string SellingPrice { get; set; }
+    }
+}
