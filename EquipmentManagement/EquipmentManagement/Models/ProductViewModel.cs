e4236d27c45467c6687d476daf5bdae282673afc
Adriangrz <adriang1412g@gmail.com>
2022-03-31 18:09:01 +0200

    Add automaper package and ProductViewModel class

---
+++ EquipmentManagement/EquipmentManagement/Models/ProductViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EquipmentManagement.Models
+{
+    public class ProductViewModel
+    {
+        public Guid Id { get; set; }
+        [Display(Name = "Nazwa")]
+        public string Name { get; set; }
+        [Display(Name = "Typ")]
+        public string Type { get; set; }
+        [Display(Name = "Opis")]
+        public string Desc { get; set; }
+        [Display(Name = "Cena")]
+        public decimal Price { get; set; }
+        [Display(Name = "Dostępność")]
+        public bool Availability { get; set; }
+    }
+}
